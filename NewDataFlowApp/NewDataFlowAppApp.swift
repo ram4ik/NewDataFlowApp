3615eb0d8447665fbf0a4e888368b9887f6bb4fe
ram4ik <ram4ik@hotmail.com>
2023-10-16 08:57:59 -0700

    Initial Commit

---
+++ NewDataFlowApp/NewDataFlowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewDataFlowAppApp.swift
+//  NewDataFlowApp
+//
+//  Created by admin on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct NewDataFlowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
